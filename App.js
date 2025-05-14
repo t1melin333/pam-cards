6d805646763bc6abfe61fd964f4c870511de9f0d
suzaneee <t1melin333@gmail.com>
2025-05-14 10:32:47 -0300

    dita 14

--- App.js
+++ App.js
@@ -1,46 +1,28 @@
 import React from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import CardPersonagem from './components/CardPersonagem';
+import Personagens from './data/personagens.json';
 
 export default function App() {
   return (
+    <View style= {styles.container}>
     <ScrollView contentContainerStyle={styles.container}>
+    <View style={styles.row}>
+      {Personagens.map((Personagens)=> (
+       <CardPersonagem
+       key={Personagens.id}
+       nome={Personagens.Nome}
+       universo={Personagens.Universo}
+       imagem={Personagens.Imagem}
+       />
+      )
       
-      <View style={styles.linha}>
-        <CardPersonagem
-          nome="Tiana"
-          universo="Disney"
-          imagem="https://m.media-amazon.com/images/M/MV5BMmRkODZlZTQtOWE3OS00ZmE1LWFiZDYtOGFjMjcwMmJjZTllXkEyXkFqcGc@._V1_.jpg"
-        />
-        <CardPersonagem
-          nome="Naveen"
-          universo="Disney"
-          imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc0cTfjpoRKFn5m8rqIxq7iRPWHGS9WFzf0w&s"
-        />
-      </View>
-
-      <View style={styles.linha}>
-        <CardPersonagem
-          nome="Dr. Facilier"
-          universo="Disney"
-          imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ1f0UQXdqUFEwvYhzeIONGiKaPSoAIybNhw&s"
-        />
-        <CardPersonagem
-          nome="Mama Odie"
-          universo="Disney"
-          imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDQUjLVflG2IqAD4eU-O8EWUq5bu5CkYr4NQ&s0"
-        />
-      </View>
-      <View>
-        <CardPersonagem
-          nome="Ray"
-          universo="Disney"
-          imagem="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Ucsr6GDlZHg-Imk7i07MneM3a3vVlwa_DA&s"
-        />
-      </View>
-    </ScrollView>
-  );
+      )
 }
+    </View>
+    </ScrollView>
+    </View>
+  )
 
 const styles = StyleSheet.create({
   container: {
@@ -56,4 +38,4 @@ const styles = StyleSheet.create({
     width: '100%',
   },
 });
-
+}
