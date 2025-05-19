a2a472ad25a0fd1a9218b814d887bb0d50dc3b7c
suzaneee <t1melin333@gmail.com>
2025-05-19 10:26:39 -0300

    dia da entrega

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import CardPersonagem from './components/CardPersonagem';
 import Personagens from './data/personagens.json';
 
 export default function App() {
   return (
+
     <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.linha}>
-        {Personagens.map((personagem) => (
-          <CardPersonagem
-            key={personagem.id}
-            nome={personagem.Nome}
-            universo={personagem.Universo}
-            imagem={personagem.Imagem}
-          />
-        ))}
-      </View>
-    </ScrollView>
+        <View style={styles.linha}>
+          {Personagens.map((personagem) => (
+            <CardPersonagem
+              key={personagem.id}
+              nome={personagem.Nome}
+              universo={personagem.Universo}
+              imagem={personagem.Imagem} />
+          ))}
+        </View>
+      </ScrollView>
   );
 }
 
 
 const styles = StyleSheet.create({
+  titulo:{
+    alignItems: 'center'
+  },
   scrollContainer: {
     paddingTop: 50,
     paddingBottom: 20,
